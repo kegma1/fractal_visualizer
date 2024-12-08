c0113d275fba1b8341ed9d1fb0bfea11f3e66f05
Kennet Mathisen <kennetam02@gmail.com>
2024-12-08 16:36:27 +0100

    updated and added controlls

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import GUI from "lil-gui";
 
 const mandelBrot = /*glsl*/ `
 precision highp float;
@@ -8,6 +9,11 @@ uniform vec2 u_center;
 uniform float u_zoom;
 uniform int u_maxIterations;
 
+uniform vec2 u_startZ;
+uniform vec2 u_startC;
+
+uniform bool u_zFocus;
+
 vec3 gradient(float t) {
     vec3 red = vec3(1.0, 0.0, 0.0);
     vec3 blue = vec3(0.0, 0.0, 1.0);
@@ -16,24 +22,37 @@ vec3 gradient(float t) {
 }
 
 void main() {
-    vec2 c = (gl_FragCoord.xy - u_resolution / 2.0) / u_zoom + u_center;
-
-    vec2 z = vec2(0.0);
     int iterations = 0;
-    for(int i = 0; i < 1000; i++) {
-        if (i == u_maxIterations) break;
-        if (dot(z, z) > 4.0) break;
-
-        // float xTemp = pow(z.x, 2.0) - pow(z.y, 2.0)  + c.x;
-        // z.y = -abs(2.0 * z.x * z.y) + c.y;
-        // z.x = xTemp;
-
-
-        float xTemp = pow(z.x, 2.0) - pow(z.y, 2.0)  + c.x;
-        z.y = 2.0 * z.x * z.y + c.y;
-        z.x = xTemp;
-
-        iterations += 1;
+    if (u_zFocus) {
+        vec2 c = (gl_FragCoord.xy - u_resolution / 2.0) / u_zoom + u_center;
+    
+        vec2 z = u_startZ;
+        for(int i = 0; i < 1000; i++) {
+            if (i == u_maxIterations) break;
+            if (dot(z, z) > 4.0) break;
+    
+         
+            float xTemp = pow(z.x, 2.0) - pow(z.y, 2.0)  + c.x;
+            z.y = 2.0 * z.x * z.y + c.y;
+            z.x = xTemp;
+    
+            iterations += 1;
+        }
+    } else {
+        vec2 z = (gl_FragCoord.xy - u_resolution / 2.0) / u_zoom + u_center;
+    
+        vec2 c = u_startC;
+        for(int i = 0; i < 1000; i++) {
+            if (i == u_maxIterations) break;
+            if (dot(z, z) > 4.0) break;
+    
+         
+            float xTemp = pow(z.x, 2.0) - pow(z.y, 2.0)  + c.x;
+            z.y = 2.0 * z.x * z.y + c.y;
+            z.x = xTemp;
+    
+            iterations += 1;
+        }
     }
 
     
@@ -100,6 +119,9 @@ const mandelBrotMat = new THREE.ShaderMaterial( {
         u_center: { value: new THREE.Vector2(0.0, 0.0) },
         u_zoom: { value: 300},
         u_maxIterations: { value: 100 },
+        u_startZ: {value: new THREE.Vector2(0.0, 0.0)},
+        u_startC: {value: new THREE.Vector2(0.0, 0.0)},
+        u_zFocus: {value: true},
     },
     fragmentShader: mandelBrot,
 })
@@ -127,11 +149,13 @@ const zoomFactor = 1.1;
 export function main() {
     window.addEventListener("resize", onWindowResize, false);
 
-    const sel = document.getElementById("fractalSelector")
-    sel.addEventListener("change", setFractal)
-    sel.value = "mandelbrot"
+    const canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
 
-    document.addEventListener("wheel", (event) => {
+    ri.renderer = new THREE.WebGLRenderer({canvas: canvas});
+    ri.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    canvas.addEventListener("wheel", (event) => {
 
 
         if (Math.sign(event.wheelDelta) < 0) {
@@ -144,11 +168,11 @@ export function main() {
         event.preventDefault();
     });
 
-    document.addEventListener("mousedown", (e) => {
+    canvas.addEventListener("mousedown", (e) => {
         ri.isPanning = true
         ri.mousePrevious.set(e.offsetX, e.offsetY);
     });
-    document.addEventListener("mousemove", (e) => {
+    canvas.addEventListener("mousemove", (e) => {
         if (ri.isPanning) {
             let mouse = new THREE.Vector2(e.offsetX, e.offsetY);
             let delta =  new THREE.Vector2();
@@ -170,13 +194,8 @@ export function main() {
             ri.mousePrevious.set(mouse.x, mouse.y);
         }
     });
-    document.addEventListener("mouseup", () =>  ri.isPanning = false);
+    canvas.addEventListener("mouseup", () =>  ri.isPanning = false);
 
-    const canvas = document.createElement("canvas");
-    document.body.appendChild(canvas);
-
-    ri.renderer = new THREE.WebGLRenderer({canvas: canvas});
-    ri.renderer.setSize(window.innerWidth, window.innerHeight);
 
     ri.material = mandelBrotMat;
 
@@ -186,6 +205,27 @@ export function main() {
     ri.scene = new THREE.Scene();
     ri.scene.background = new THREE.Color( 0xdddddd );
     ri.scene.add(ri.plane);
+
+    ri.gui = new GUI();
+    ri.gui.add(ri.material.uniforms.u_zFocus, "value").name("Z/C");
+
+    const zFolder = ri.gui.addFolder("Z")
+    const zControlX = zFolder.add(ri.material.uniforms.u_startZ.value, "x").min(-1).max(1).step(0.01).name("X");
+    const zControlY = zFolder.add(ri.material.uniforms.u_startZ.value, "y").min(-1).max(1).step(0.01).name("Y");
+    zFolder.add({func: () => {
+        ri.material.uniforms.u_startZ.value.set(0.0, 0.0)
+        zControlY.updateDisplay();
+        zControlX.updateDisplay();
+    }}, "func").name("Reset");
+    
+    const cFolder = ri.gui.addFolder("C")
+    const cControlX = cFolder.add(ri.material.uniforms.u_startC.value, "x").min(-1).max(1).step(0.01).name("X");
+    const cControlY = cFolder.add(ri.material.uniforms.u_startC.value, "y").min(-1).max(1).step(0.01).name("Y");
+    cFolder.add({func: () => {
+        ri.material.uniforms.u_startC.value.set(0.0, 0.0)
+        cControlY.updateDisplay();
+        cControlX.updateDisplay();
+    }}, "func").name("Reset");
 
     ri.camera = new THREE.Camera();
    
